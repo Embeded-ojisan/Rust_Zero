aa9602970aa6cf28edba15071764b9b32719121f
lowlevelengineer <lowlevelengineer@gmail.com>
2023-05-16 23:27:25 +0900

    shell追加

--- 7_ch/zrsh/src/shell.rs
+++ 7_ch/zrsh/src/shell.rs
@@ -284,6 +284,357 @@ impl Worker {
         self.set_pid_state(pid, ProcState::Run);
     }
 
-    fn
+    fn process_term(&mut self, pid: Pid, shell_tx: &SyncSender<ShellMsg>) {
+        if let Some((job_id, pgid)) = self.remove_pid(pid) {
+            self.manage_job(job_id, pgid, shell_tx);
+        }
+    }
+    fn set_pid_state(&mut self, pid: Pid, state: ProcState) -> Option<ProcState> {
+        let info = self.pid_to_info.get_mut(&pid)?;
+        Some(replace(&mut info.state, state))
+    }
+
+    fn remove_pid(&mut self, pid: Pid) -> Option<(usize, Pid)> {
+        let pgid = self.pid_to_info.get(&pid)?.pgid;
+        let it = self.pgid_to_pids.get_mut(&pgid)?;
+        it.1.remove(&pid);
+        let job_id = it.0;
+        Some((job_id, pgid))
+    }
+
+    fn remove_job(&mut self, job_id: usize) {
+        if let Some((pgid, _)) = self.jobs.remove(&job_id) {
+            if let Some((_, pids)) = self.pgid_to_pids.remove(&pgid) {
+                assert!(pids.is_empty());
+            }
+        }
+    }
+
+    fn is_group_empty(&self, pgid: Pid) -> bool {
+        self.pgid_to_pids.get(&pgid).unwrap().1.is_empty()
+    }
+
+    fn is_group_stop(&self, pgid: Pid) -> Option<bool> {
+        for pid in self.pgid_to_pids.get(&pgid)?.1.iter() {
+            if self.pid_to_info.get(pid).unwrap().state == ProcState::Run {
+                return Some(false);
+            }
+        }
+        Some(true)
+    }
+
+    fn get_new_job_id(&self) -> Option<usize> {
+        for i in 0..=usize::MAX {
+            if !self.jobs.contains_key(&i) {
+                return Some(i);
+            }
+        }
+        None
+    }
+
+    fn insert_job(&mut self, job_id: usize, pgid: Pid, pids: HashMap<Pid, ProcInfo>, line: &str) {
+        assert!(!self.jobs.contains_key(&job_id));
+        self.jobs.insert(job_id, (pgid, line.to_string()));
+
+        let mut procs = HashSet::new();
+        for (pid, info) in pids {
+            procs.insert(pid);
+
+            assert!(!self.pid_to_info.contains_key(&pid));
+            self.pid_to_info.insert(pid, info);
+        }
+
+        assert!(!self.pgid_to_pids.contains_key(&pgid));
+        self.pgid_to_pids.insert(pgid, (job_id, procs));
+    }
+
+    fn set_shell_fg(&mut self, shell_tx: &SyncSender<ShellMsg>) {
+        self.fg = None;
+        tcsetpgrp(libc::STDIN_FILENO, self.shell_pgid).unwrap();
+        shell_tx.send(ShellMsg::Continue(self.exit_val)).unwrap();
+    }
+
+    fn manage_job(&mut self, job_id: usize, pgid: Pid, shell_tx: &SyncSender<ShellMsg>) {
+        let is_fg = self.fg.map_or(false, |x| pgid == x);
+        let line = &self.jobs.get(&job_id).unwrap().1;
+        if is_fg {
+            if self.is_group_empty(pgid) {
+                eprintln!("[{job_id}] 終了\t{line}");
+                self.remove_job(job_id);
+                self.set_shell_fg(shell_tx);
+            } else if self.is_group_stop(pgid).unwrap() {
+                eprintln!("\n[{job_id}] 停止\t{line}");
+                self.set_shell_fg(shell_tx);
+            }
+        } else {
+            if self.is_group_empty(pgid) {
+                eprintln!("\n[{job_id}] 終了\t{line}");
+                self.remove_job(job_id);
+            }
+        }
+    }
+
+    fn spawn_child(&mut self, line: &str, cmd: &[(&str, Vec<&str>)]) -> bool {
+        assert_ne!(cmd.len(), 0);
+
+        let job_id = if let Some(id) = self.get_new_job_id() {
+            id
+        } else {
+            eprintln!("ZeroSh: 管理可能なジョブの最大値に到達");
+            return false;
+        };
+
+        if cmd.len() > 2 {
+            eprintln!("ZeroSh: 三つ以上のコマンドによるパイプはサポートしていません");
+            return false;
+        }
+
+        let mut input = None;
+        let mut output = None;
+        if cmd.len() == 2 {
+            let p = pipe().unwrap();
+            input = Some(p.0);
+            output = Some(p.1);
+        }
+
+        let cleanup_pipe = CleanUp {
+            f: || {
+                if let Some(fd) = input {
+                    syscall(|| unistd::close(fd)).unwrap();
+                }
+                if let Some(fd) = output {
+                    syscall(|| unistd::close(fd)).unwrap();
+                }
+            },
+        };
+
+        let pgid;
+        match fork_exec(Pid::from_raw(0), cmd[0].0, &cmd[0].1, None, output) {
+            Ok(child) => {
+                pgid = child;
+            }
+            Err(e) => {
+                eprintln!("ZeroSh: プロセス生成エラー: {e}");
+                return false;
+            }
+        }
+
+        let info = ProcInfo {
+            state: ProcState::Run,
+            pgid,
+        };
+        let mut pids = HashMap::new();
+        pids.insert(pgid, info.clone());
+
+        if cmd.len() == 2 {
+            match fork_exec(pgid, cmd[1].0, &cmd[1].1, input, None) {
+                Ok(child) => {
+                    pids.insert(child, info);
+                }
+                Err(e) => {
+                    eprintln!("ZeroSh: プロセス生成エラー: {e}");
+                    return false;
+                }
+            }
+        }
+
+        std::mem::drop(cleanup_pipe);
+
+        self.fg = Some(pgid);
+        self.insert_job(job_id, pgid, pids, line);
+        tcsetpgrp(libc::STDIN_FILENO, pgid).unwrap();
+
+        true
+    }
+
+    fn built_in_cmd(&mut self, cmd: &[(&str, Vec<&str>)], shell_tx: &SyncSender<ShellMsg>) -> bool {
+        if cmd.len() > 1 {
+            return false;
+        }
+
+        match cmd[0].0 {
+            "exit" => self.run_exit(&cmd[0].1, shell_tx),
+            "jobs" => self.run_jobs(shell_tx),
+            "fg" => self.run_fg(&cmd[0].1, shell_tx),
+            "cd" => self.run_cd(&cmd[0].1, shell_tx),
+            _ => false,
+        }
+    }
+
+    fn run_cd(&mut self, args: &[&str], shell_tx: &SyncSender<ShellMsg>) -> bool {
+        let path = if args.len() == 1 {
+            dirs::home_dir()
+                .or_else(|| Some(PathBuf::from("/")))
+                .unwrap()
+        } else {
+            PathBuf::from(args[1])
+        };
+
+        if let Err(e) = std::env::set_current_dir(&path) {
+            self.exit_val = 1;
+            eprintln!("cdに失敗: {e}");
+        } else {
+            self.exit_val = 0;
+        }
+
+        shell_tx.send(ShellMsg::Continue(self.exit_val)).unwrap();
+        true
+    }
+
+    fn run_exit(&mut self, args: &[&str], shell_tx: &SyncSender<ShellMsg>) -> bool {
+        if !self.jobs.is_empty() {
+            eprintln!("ジョブが実行中なので終了できません");
+            self.exit_val = 1;
+            shell_tx.send(ShellMsg::Continue(self.exit_val)).unwrap();
+            return true;
+        }
+
+        let exit_val = if let Some(s) = args.get(1) {
+            if let Ok(n) = (*s).parse::<i32>() {
+                n
+            } else {
+                eprintln!("{s}は不正な引数です");
+                self.exit_val = 1; // 失敗
+                shell_tx.send(ShellMsg::Continue(self.exit_val)).unwrap();
+                return true;
+            }
+        } else {
+            self.exit_val
+        };
+
+        shell_tx.send(ShellMsg::Quit(exit_val)).unwrap();
+        true
+    }
+
+    fn run_jobs(&mut self, shell_tx: &SyncSender<ShellMsg>) -> bool {
+        for (job_id, (pgid, cmd)) in &self.jobs {
+            let state = if self.is_group_stop(*pgid).unwrap() {
+                "停止中"
+            } else {
+                "実行中"
+            };
+            println!("[{job_id}] {state}\t{cmd}")
+        }
+        self.exit_val = 0;
+        shell_tx.send(ShellMsg::Continue(self.exit_val)).unwrap();
+        true
+    }
+
+    fn run_fg(&mut self, args: &[&str], shell_tx: &SyncSender<ShellMsg>) -> bool {
+        self.exit_val = 1;
+
+        if args.len() < 2 {
+            eprintln!("usage: fg 数字");
+            shell_tx.send(ShellMsg::Continue(self.exit_val)).unwrap();
+            return true;
+        }
+
+        if let Ok(n) = args[1].parse::<usize>() {
+            if let Some((pgid, cmd)) = self.jobs.get(&n) {
+                eprintln!("[{n}] 再開\t{cmd}");
+
+                self.fg = Some(*pgid);
+                tcsetpgrp(libc::STDIN_FILENO, *pgid).unwrap();
+
+                killpg(*pgid, Signal::SIGCONT).unwrap();
+                return true;
+            }
+        }
+
+        eprintln!("{}というジョブは見つかりませんでした", args[1]);
+        shell_tx.send(ShellMsg::Continue(self.exit_val)).unwrap();
+        true
+    }
 }
 
+fn syscall<F, T>(f: F) -> Result<T, nix::Error>
+where
+    F: Fn() -> Result<T, nix::Error>,
+{
+    loop {
+        match f() {
+            Err(nix::Error::EINTR) => (),
+            result => return result,
+        }
+    }
+}
+
+fn fork_exec(
+    pgid: Pid,
+    filename: &str,
+    args: &[&str],
+    input: Option<i32>,
+    output: Option<i32>,
+) -> Result<Pid, DynError> {
+    let filename = CString::new(filename).unwrap();
+    let args: Vec<CString> = args.iter().map(|s| CString::new(*s).unwrap()).collect();
+
+    match syscall(|| unsafe { fork() })? {
+        ForkResult::Parent { child, .. } => {
+            setpgid(child, pgid).unwrap();
+            Ok(child)
+        }
+        ForkResult::Child => {
+            setpgid(Pid::from_raw(0), pgid).unwrap();
+
+            if let Some(infd) = input {
+                syscall(|| dup2(infd, libc::STDIN_FILENO)).unwrap();
+            }
+            if let Some(outfd) = output {
+                syscall(|| dup2(outfd, libc::STDOUT_FILENO)).unwrap();
+            }
+
+            for i in 3..=6 {
+                let _ = syscall(|| unistd::close(i));
+            }
+
+            match execvp(&filename, &args) {
+                Err(_) => {
+                    unistd::write(libc::STDERR_FILENO, "不明なコマンドを実行\n".as_bytes()).ok();
+                    exit(1);
+                }
+                Ok(_) => unreachable!(),
+            }
+        }
+    }
+}
+
+fn parse_cmd_one(line: &str) -> Result<(&str, Vec<&str>), DynError> {
+    let cmd: Vec<&str> = line.split(' ').collect();
+    let mut filename = "";
+    let mut args = Vec::new();
+    for (n, s) in cmd.iter().filter(|s| !s.is_empty()).enumerate() {
+        if n == 0 {
+            filename = *s;
+        }
+        args.push(*s);
+    }
+
+    if filename.is_empty() {
+        Err("空のコマンド".into())
+    } else {
+        Ok((filename, args))
+    }
+}
+
+fn parse_pipe(line: &str) -> Vec<&str> {
+    let cmds: Vec<&str> = line.split('|').collect();
+    cmds
+}
+
+type CmdResult<'a> = Result<Vec<(&'a str, Vec<&'a str>)>, DynError>;
+
+fn parse_cmd(line: &str) -> CmdResult {
+    let cmds = parse_pipe(line);
+    if cmds.is_empty() {
+        return Err("空のコマンド".into());
+    }
+
+    let mut result = Vec::new();
+    for cmd in cmds {
+        let (filename, args) = parse_cmd_one(cmd)?;
+        result.push((filename, args));
+    }
+
+    Ok(result)
+}
